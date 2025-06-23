234b221bb59a06f0973a0855a58666c0c2b1af6e
bowligth <pdietzel98@gmail.com>
2025-06-23 15:58:02 +0200

    frontend komplett überarbeitet

---
+++ planifyapp/frontend/src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Planify Frontend</h1>
+      <p>Dein Projekt ist startklar!</p>
+    </div>
+  );
+}
+
+export default App;
